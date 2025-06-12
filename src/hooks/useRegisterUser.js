a0282fb7c2a17ee5332a448e7d8d6344242347d4
Dipendra <hehesuiii69@gmail.com>
2025-06-12 17:18:00 +0545

    toast msg error solved

--- src/hooks/useRegisterUser.js
+++ src/hooks/useRegisterUser.js
@@ -12,7 +12,7 @@ export const useRegisterUser = () => {
   return useMutation({
     mutationFn: registerUserService, // Using the imported function here
     onSuccess: (data) => {
-      toast.success(data.message || 'Signup successful! You can now log in.');
+    //   toast.success(data.message || 'Signup successful! You can now log in.');
     },
     onError: (error) => {
       toast.error(error.message || 'Signup failed.');
